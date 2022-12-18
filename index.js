e49e72841631ed8c8166979490643809e5dbb760
Allan Monzon <greengamer.am@gmail.com>
2022-12-18 14:52:35 -0500

    commit tree 12a7f5f9a344c51a3ff0235faf21a75d960ca05e

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+main.remove()
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Allan is the champion this happend before right'
